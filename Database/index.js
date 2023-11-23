7eac60238d4355c4ed670b8694b7ba44a7d4090b
Elijah Kahn-Ihejiere <elijah.kihejiere@gmail.com>
2023-11-22 19:02:46 -0500

    change to Database index.js

--- Database/index.js
+++ Database/index.js
@@ -1,9 +1,15 @@
-import courses from "./courses.json" assert { type: "json" };
-import modules from "./modules.json" assert { type: "json" };
-import assignments from "./assignments.json" assert { type: "json" };
-import users from "./users.json" assert { type: "json" };
-import grades from "./grades.json" assert { type: "json" };
-import enrollment from "./enrollment.json" assert { type: "json" };
+import { readFile } from 'fs/promises';
+async function loadJSON(filename) {
+  const content = await readFile(filename, 'utf8');
+  return JSON.parse(content);
+}
+const courses = await loadJSON(new URL('./courses.json', import.meta.url));
+const modules = await loadJSON(new URL('./modules.json', import.meta.url));
+const assignments = await loadJSON(new URL('./assignments.json', import.meta.url));
+const users = await loadJSON(new URL('./users.json', import.meta.url));
+const grades = await loadJSON(new URL('./grades.json', import.meta.url));
+const enrollment = await loadJSON(new URL('./enrollment.json', import.meta.url));
+
 export default {
  courses,
  modules, 
