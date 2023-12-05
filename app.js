84bf171f880531dd4035cbcb1893807e6f4085ea
Elijah Kahn-Ihejiere <elijah.kihejiere@gmail.com>
2023-12-05 01:07:59 -0500

    a6 Mongo fa23

--- app.js
+++ app.js
@@ -5,11 +5,45 @@ import Hello from "./hello.js"
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from './modules/routes.js';
 import cors from "cors";
+import mongoose from "mongoose";
+import UserRoutes from './users/routes.js';
+import "dotenv/config";
+import session from "express-session";
+
+const DB_CONNECTION_STRING = "mongodb+srv://ElijahK:8A5OjYfJUQfV3IlO@cluster0.z1np8ck.mongodb.net/kanbas?retryWrites=true&w=majority"
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
+
+mongoose.connect(CONNECTION_STRING);
 const app = express();
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:3000",
+      origin: process.env.FRONTEND_URL
+    })
+   );
+
+
+   const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+  
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
 Hello(app);
 Lab5(app);
+UserRoutes(app);
+
 app.listen(process.env.PORT || 4000);
